1f328ffd442840e8fab5609da2610e030861e127
Sree-cool <78275933+Sree-cool@users.noreply.github.com>
2021-09-20 14:21:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 Webcam.set({
-    width: 350,
+    width: 300,
     height: 300,
     image_format: 'png',
     png_quality: 90
 });
+
 Webcam.attach('#camera');
 function take_snapshot(){
     Webcam.snap(function (data_uri){
-        document.getElementById("result").innerHTML = '<img id="capture_image"src="'+data_uri+uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
     
     }
 console.log('ml5 version:',ml5.version);
+
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/rdtZxelbZ/model.json',modelLoaded);
+
 function modelLoaded(){
     console.log('model loaded')
 };
+
+function check()
+{
+    img = document.getElementById("caputured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
